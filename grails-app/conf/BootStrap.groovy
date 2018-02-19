dab71955b4609c1d5d10c2035d37a9b2826ffcbe
mburns <mike.burns@afoundria.com>
2018-02-19 23:58:41 +0000

    Create 10 breeds

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,19 @@
+import kittens.*
+
 class BootStrap {
 
     def init = { servletContext ->
-        log.info("Bootstrap running")
+        log.info("creating 10 breeds")
+
+        def breeds=[]
+
+        if (Breed.count()==0) {
+            10.times {
+                breeds << new Breed(description: "Breed $it").save(failOnError:true, flush:true)
+            }
+        } else {
+            breeds=Breed.list()
+        }
     }
     def destroy = {
     }
