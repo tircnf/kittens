8ea87b9a7bcdc1b2bd0bf40b1aa35dbde84b0512
mburns <mike.burns@afoundria.com>
2018-02-20 06:27:55 +0000

    Stub in a bunch of search options.

--- grails-app/controllers/kittens/KittenController.groovy
+++ grails-app/controllers/kittens/KittenController.groovy
@@ -14,11 +14,20 @@ class KittenController {
     def test1="max=10&offset=100"
     def test2="max=10&offset=100&kittenName=2"
 
-
     def theTest=test1
 
 
     def index() {
+
+        def searches=[
+            ["A basic where query",  "where"],
+            ["A basic Finder",       "finder"],
+            ["Detached Criteria",    "detachedCriteria"],
+            ["Criteria Query",       "criteria"],
+            ["HQL Query",            "hql"],
+            ["Plain old Sql",        "sql"],
+        ]
+
         render {
             html {
                 body {
@@ -26,9 +35,13 @@ class KittenController {
                     a(href:"http://thecatapi.com") {
                         delegate.img(src:"http://thecatapi.com/api/images/get?format=src&type=gif")
                     }
-                    p("Try a basic where query")
-                    a(href:"./where?max=10&offset=100") {
-                        pre("curl http://localhost:8080/kittens/kitten/where?$theTest")
+
+                    searches.each { searchInfo->
+                        hr("")
+                        p("${searchInfo[0]}")
+                        a(href:"./${searchInfo[1]}.xml?max=10&offset=100") {
+                            pre("curl http://localhost:8080/kittens/kitten/${searchInfo[1]}?$theTest")
+                        }
                     }
                 }
             }
@@ -38,15 +51,52 @@ class KittenController {
 
     def where(KittenSearchCommand ks) {
 
-        log.info("index called");
-        log.info("ks = ${ks.properties}");
+        log.info("using where query  ${ks.properties}")
 
-        def result=Kitten.where {
+        def pagedResult=Kitten.where {
 
-        }.list(max: ks.max, offset: ks.offset).collect {
+        }.list(max: ks.max, offset: ks.offset)
+
+
+        def result=pagedResult.collect {
             [id: it.id, name: it.name, breed_id: it.breed.id, breed: it.breed.description, person: it.person.name, building: it.person.building.zipCode]
         }
 
-        respond ([total: result.totalCount, data: result])
+        respond ([total: pagedResult.getTotalCount(), data: result])
     }
+
+    def finder(KittenSearchCommand ks) {
+        log.info("using finder  ${ks.properties}")
+
+        respond ([total: 0, data: []]);
+    }
+
+
+    def detachedCriteria(KittenSearchCommand ks) {
+        log.info("using detachedCriteria  ${ks.properties}")
+
+        respond ([total: 0, data: []]);
+    }
+
+
+    def criteria(KittenSearchCommand ks) {
+        log.info("using criteria  ${ks.properties}")
+
+        respond ([total: 0, data: []]);
+    }
+
+
+    def hql(KittenSearchCommand ks) {
+        log.info("using hql  ${ks.properties}")
+
+        respond ([total: 0, data: []]);
+    }
+
+
+    def sql(KittenSearchCommand ks) {
+        log.info("using sql  ${ks.properties}")
+
+        respond ([total: 0, data: []]);
+    }
+
 }
